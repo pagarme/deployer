25e37363b967d78233a87c4a9ff11af6caeb924c
Jonathan Lima <greenboxal@gmail.com>
2017-05-24 20:18:36 -0300

    Update vendor

---
+++ vendor/github.com/hashicorp/consul/command/keygen_test.go
@@ -0,0 +1,37 @@
+package command
+
+import (
+	"encoding/base64"
+	"testing"
+
+	"github.com/hashicorp/consul/command/base"
+	"github.com/mitchellh/cli"
+)
+
+func TestKeygenCommand_implements(t *testing.T) {
+	var _ cli.Command = &KeygenCommand{}
+}
+
+func TestKeygenCommand(t *testing.T) {
+	ui := new(cli.MockUi)
+	c := &KeygenCommand{
+		Command: base.Command{
+			UI:    ui,
+			Flags: base.FlagSetNone,
+		},
+	}
+	code := c.Run(nil)
+	if code != 0 {
+		t.Fatalf("bad: %d", code)
+	}
+
+	output := ui.OutputWriter.String()
+	result, err := base64.StdEncoding.DecodeString(output)
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	if len(result) != 16 {
+		t.Fatalf("bad: %#v", result)
+	}
+}
