c22fd15b2e9cb258d683942927e7b887be1f4be2
Vidal Vasconcelos <vasconcelosvidal@gmail.com>
2021-06-05 13:37:09 -0300

    refactor(logger): extract config variable
    Separating the code that doesn't produce errors from the part that
    does can make the code easier to read.

--- logger/dynamodb.go
+++ logger/dynamodb.go
@@ -28,15 +28,17 @@ func (d *DynamoLogger) Init() {
 		os.Exit(1)
 	}
 
-	session, err := session.NewSession(&aws.Config{
+	config := &aws.Config{
 		Region: aws.String(awsRegion),
-	})
+	}
+
+	ses, err := session.NewSession(config)
 	if err != nil {
 		fmt.Printf("An error occured when starting the AWS Session: %s\n", err)
 		os.Exit(1)
 	}
 
-	d.Svc = dynamodb.New(session)
+	d.Svc = dynamodb.New(ses)
 	d.Table = aws.String(dynamoTable)
 }
 
